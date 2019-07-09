d9dad10f9bbcbc322ffcc392e52d030d68d1f5f0
Edward Iga Kigongo <igakigongo@gmail.com>
2019-07-09 18:33:54 +0200

    Add contact us static page and test to confirm we can get it via http

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,6 +12,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
