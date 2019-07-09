725554955b784c39793bac273a33d26e30ff2396
Edward Iga Kigongo <igakigongo@gmail.com>
2019-07-09 18:46:31 +0200

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -29,4 +29,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
+
+  test "should get root" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
 end
