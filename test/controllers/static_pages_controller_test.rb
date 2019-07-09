333464058bacc7460efb1532fd6421143ab19974
Edward Iga Kigongo <igakigongo@gmail.com>
2019-07-09 18:36:54 +0200

    Use layout with contact page - passes title tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,17 +15,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-  end
-
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 end
