2e50eb15592635ffc8f467976a0a363adafec11f
Edward Iga Kigongo <igakigongo@gmail.com>
2019-07-22 17:11:37 +0200

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for(user, size: 50)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = !user.email.nil? ? "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}" : "https://www.gravatar.com/avatar/?d=identicon&size=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'foo/bar'
-  get 'foo/baz'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
-  get '/help', to: 'static_pages#help'
-  root 'static_pages#home'
+  root "static_pages#home"
+  get "users/new"
+  get "foo/bar"
+  get "foo/baz"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  get "/help", to: "static_pages#help"
+  resources :users
 end
